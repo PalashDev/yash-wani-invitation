d5098bc8cf22a66ea5858de3dab7f8ef5bdbe2cc
PalashDev <palash.yo@gmail.com>
2023-12-11 15:20:18 +0530

    fg

--- src/model/Event.model.ts
+++ src/model/Event.model.ts
@@ -1,8 +1,9 @@
 export interface Event {
      Name :string;
      Desc : string;
-     date : Date 
-     time : string
-     place: string
-     
+     date : Date;
+     time : string;
+     place: string;
+     howToReach: string;
+     mapsLocation:string
 }
